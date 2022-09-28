618c81c5278dc019db6c5f50d782eb1b61cc2095
brandon <bleatherman01@gmail.com>
2022-09-28 16:25:13 -0400

    First commit Power Table

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace Powers_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!");
+            
+            bool keepGoing = true;
+
+            while (keepGoing == true)
+            {
+                Console.WriteLine("Please enter a positive integer");
+                
+                string input = Console.ReadLine();
+                int num = int.Parse(input);
+
+               Console.WriteLine("Number\tSquared\tCubed \n======= ======= =====");
+
+                for (int i = 1; i <= num; i++)
+                {
+                   Console.WriteLine($"{i} \t{i * i} \t{i * i * i}");
+                }
+           
+               Console.WriteLine("\nWould you like to check a new number? Y/N");
+               string goOn = Console.ReadLine().ToLower();
+
+                if (goOn == "y")
+                {
+                    continue;
+                }
+                else if (goOn == "n")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    Console.WriteLine("I didn't understand that let's try that again");
+                }
+            }
+        }
+    }
+}
